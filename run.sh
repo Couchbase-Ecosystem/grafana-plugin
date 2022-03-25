94cb5753a9178354baf89a70ce9e8d848c6236bb
chedim <>
2022-03-25 12:32:42 -0400

    test stand

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : chedim (chedim@couchbaser)
+# @file        : run
+# @created     : Friday Mar 25, 2022 11:41:40 EDT
+#
+# @description : Starts grafana with the plugin
+######################################################################
+
+docker-compose up 
