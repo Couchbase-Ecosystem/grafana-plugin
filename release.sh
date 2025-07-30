4586cbc6b5c72f8e8b113aed2f0b3065f53e9336
Dmitrii Chechetkin <dmitrii.chechetkin@couchbase.com>
2025-07-30 09:36:28 -0400

    1.2.1: support for grafana repositories

--- release.sh
+++ release.sh
@@ -10,6 +10,7 @@
 
 
 tmp=$(mktemp -d)
+CBVER=$(cat couchbase-datasource/package.json | jq -r .version)
 
 mkdir "$tmp/couchbase-datasource"
 
@@ -21,3 +22,9 @@ cp -r dist "$tmp/couchbase-datasource/"
 popd
 
 zip couchbase-datasource.zip "$tmp/couchbase-datasource" -r
+
+mkdir -p plugins/couchbase-datasource/versions/${CBVER}
+cp couchbase-datasource.zip plugins/couchbase-datasource/versions/${CBVER}
+
+cat plugins/repo/couchbase-datasource/index.html | jq ".versions+=[{\"arch\":{\"any\":{}},\"version\":\"${CBVER}\"}]" > "$tmp/cbds-versions.json"
+mv "$tmp/cbds-versions.json" plugins/repo/couchbase-datasource/index.html
